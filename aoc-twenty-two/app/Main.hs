9eed59d4c613677126a16775c8e2e7f1fdf0ba3f
certainty <david.krentzlin@gmail.com>
2022-12-03 12:33:25 +0100

    Setup basic structure

--- aoc-twenty-two/app/Main.hs
+++ aoc-twenty-two/app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
-import Lib
+import qualified AOC.Day1 as Day1
+import Options.Applicative
+
+data Options = Options
+  { _day1 :: Bool
+  }
+
+parseOpts :: Parser Options
+parseOpts = Options <$> switch (long "day1" <> help "Run day 1")
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= run
+  where
+    opts = info (parseOpts <**> helper) (fullDesc <> progDesc "Run Advent of Code 2020")
+
+run :: Options -> IO ()
+run Options {..}
+  | _day1 = Day1.run
+  | otherwise = putStrLn "No day"
---
+++ aoc-twenty-two/src/AOC/Day1.hs
@@ -0,0 +1,4 @@
+module AOC.Day1 (run) where
+
+run :: IO ()
+run = putStrLn "Day 1"
