4a9f097e5a6e19e524ca727f52e7e4446a7cddf7
certainty <david.krentzlin@gmail.com>
2023-12-03 20:27:15 +0100

    day3: part two
    I think this is really way to complicated.
    I regret the decision to use character matrix rather than a vector of strings.

--- 2023/day3.lisp
+++ 2023/day3.lisp
@@ -23,6 +23,19 @@
                    (setf number-part-p nil))))
         finally (return result)))
 
+(defun day3-part-two ()
+  (loop with result = 0
+        with matrix = (input-matrix)
+        for row from 0 below *matrix-size*
+        do (loop for column from 0 below *matrix-size*
+                 for cur = (aref matrix row column)
+                 when (char= #\* cur)
+                   do
+                      (a:when-let ((adjacent (adjacent-numbers matrix row column)))
+                        (when (= (length adjacent) 2)
+                          (incf result (apply #'* adjacent)))))
+        finally (return result)))
+
 (defun input-matrix ()
   (with-open-file (stream (truename "input/day3.input"))
     (let ((chars (loop for line = (read-line stream nil)
@@ -37,19 +50,67 @@
   (and (not (digit-char-p c)) (not (char= c #\.))))
 
 (defun adjacent-symbol-p (matrix row column)
+  (loop for (r  c) in (adjacent-coordinates row column)
+        thereis (symbol-p (aref matrix r c))))
+
+(defun adjacent-coordinates (row column)
+  "Returns a list of valid coordinates adjacent to the given one."
   (let ((above (1- row))
         (below (1+ row))
         (left (1- column))
         (right (1+ column)))
-    (or
-     (and (>= above 0) (symbol-p (aref matrix above column)))
-     (and (>= above 0) (>= left 0) (symbol-p (aref matrix above left)))
-     (and (>= above 0) (<= right (1- *matrix-size*)) (symbol-p (aref matrix above right)))
-     (and (>= left 0) (symbol-p (aref matrix row left)))
-     (and (<= right (1- *matrix-size*)) (symbol-p (aref matrix row right)))
-     (and (<= below (1- *matrix-size*)) (symbol-p (aref matrix below column)))
-     (and (<= below (1- *matrix-size*)) (>= left 0) (symbol-p (aref matrix below left)))
-     (and (<= below (1- *matrix-size*)) (<= right (1- *matrix-size*)) (symbol-p (aref matrix below right))))))
+    `(,@(and (>= above 0) (list (list above column)))
+      ,@(and (>= above 0) (>= left 0) (list (list above left)))
+      ,@(and (>= above 0) (<= right (1- *matrix-size*)) (list (list above right)))
+      ,@(and (>= left 0) (list (list row left)))
+      ,@(and (<= right (1- *matrix-size*)) (list (list row right)))
+      ,@(and (<= below (1- *matrix-size*)) (list (list below column)))
+      ,@(and (<= below (1- *matrix-size*)) (>= left 0) (list (list below left)))
+      ,@(and (<= below (1- *matrix-size*)) (<= right (1- *matrix-size*)) (list (list below right))))))
 
-(define-test day3-part-one-test
-    (assert-equal 557705 (day3-part-one)))
+(defun adjacent-numbers (matrix row column)
+  (let ((numbers nil))
+    (dolist (coord (adjacent-coordinates row column))
+      (let ((r (first coord))
+            (c (second coord)))
+        (when (digit-char-p (aref matrix r c))
+          (push (scan-number matrix r c) numbers))))
+    (remove-duplicates numbers :test #'=)))
+
+(defun scan-number (matrix row column)
+  "given the start coordinates scan the whole number. Numbers can span multiple lines."
+  (let ((left nil)
+        (right nil))
+    ;; left
+    (let ((r row)
+          (c (max 0 (1- column))))
+      (loop
+        (unless (digit-char-p (aref matrix r c))
+          (return))
+        (push (aref matrix r c) left)
+        (cond
+          ((and (zerop c) (zerop r))
+           (return))
+          ((zerop c)
+           (decf r)
+           (setf c (1- *matrix-size*)))
+          (t (decf c)))))
+
+    ;; right
+    (let ((r row)
+          (c column))
+      (loop
+        (unless (digit-char-p (aref matrix r c))
+          (return))
+        (push (aref matrix r c) right)
+        (cond
+          ((and (= (1- *matrix-size*) c) (>= (1+ r) *matrix-size*))
+           (return))
+          ((= (1- *matrix-size*) c)
+           (incf r)
+           (setf c 0))
+          (t (incf c)))))
+    (parse-integer (coerce (append left (reverse right)) 'string))))
+
+(define-test day3-part-one-test ()
+  (assert-equal 557705 (day3-part-one)))
