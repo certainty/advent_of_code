d9a860ae64ae1111b244c8b39eadc4a053df1953
certainty <david.krentzlin@gmail.com>
2023-12-04 15:20:49 +0100

    cleanup

--- 2023/day4.lisp
+++ 2023/day4.lisp
@@ -1,14 +1,16 @@
 (in-package :aoc.2023)
+
 (defparameter *table-size* 213)
 
 (defun day4-part-one ()
   (loop for line in (file-lines #p"input/day4.input")
-        sum (process-line line)))
+        for winners = (parse-line line)
+        sum (if (>= winners 1) (expt 2 (1- winners)) winners)))
 
 (defun day4-part-two ()
   (let ((instances (make-array *table-size* :initial-element 1)))
     (dolist (line (file-lines #p"input/day4.input"))
-      (multiple-value-bind (game-id winners) (parse-line line)
+      (multiple-value-bind (winners game-id) (parse-line line)
         (let ((copies (aref instances (1- game-id))))
           (when (and (> copies 0) (> winners 0))
             ;; forward propagate the copies
@@ -17,14 +19,6 @@
                     do (incf (aref instances i))))))))
     (reduce #'+ instances)))))
 
-(defun process-line (line)
-  (multiple-value-bind (game-id winners) (parse-line line)
-    (declare (ignore game-id))
-    (cond
-      ((zerop winners) 0)
-      ((= 1 winners) 1)
-      (t (expt 2 (1- winners))))))
-
 (defun parse-line (line)
   (destructuring-bind (game-segment data-segment) (cl-ppcre:split ":\\s+" line)
     (destructuring-bind (winning-segment my-segment) (str:split "|" data-segment)
@@ -32,7 +26,7 @@
             (my-cards       (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim my-segment)))))
         (let ((game-id (parse-integer (cadr (cl-ppcre:split "\\s+" game-segment))))
               (winners (loop for card in my-cards counting (s:true (find card winning-cards)))))
-          (values game-id  winners))))))
+          (values winners game-id))))))
 
 (define-test day4-part-one-works ()
   (assert-equal 25183 (day4-part-one)))
