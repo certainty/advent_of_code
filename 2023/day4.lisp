761acee06b07375f29c25bfc2b0d72abc9708eb8
certainty <david.krentzlin@gmail.com>
2023-12-04 15:06:03 +0100

    day4: part 2

--- 2023/day4.lisp
+++ 2023/day4.lisp
@@ -1,20 +1,38 @@
 (in-package :aoc.2023)
+(defparameter *table-size* 213)
 
 (defun day4-part-one ()
   (loop for line in (file-lines #p"input/day4.input")
         sum (process-line line)))
 
+(defun day4-part-two ()
+  (let ((instances (make-array *table-size* :initial-element 1)))
+    (dolist (line (file-lines #p"input/day4.input"))
+      (multiple-value-bind (game-id winners) (parse-line line)
+        (let ((copies (aref instances (1- game-id))))
+          (when (and (> copies 0) (> winners 0))
+            ;; forward propagate the copies
+            (dotimes (c copies)
+              (loop for i from game-id to (min (+ game-id (1- winners)) (1- *table-size*)) ; don't execeed table boundaries
+                    do (incf (aref instances i))))))))
+    (reduce #'+ instances)))))
+
 (defun process-line (line)
-  (let ((colon (position #\: line)))
-    (destructuring-bind (winning-segment my-segment) (str:split "|" (subseq line (1+ colon)))
-      (let ((winning-cards (coerce (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim winning-segment))) 'vector))
-            (my-cards      (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim my-segment)))))
-        ;; linear scan should be fast enough and we don't bother using a set or hashmap
-        (let ((winners (loop for card in my-cards counting (s:true (find card winning-cards)))))
-          (cond
-            ((zerop winners) 0)
-            ((= 1 winners) 1)
-            (t (expt 2 (1- winners)))))))))
+  (multiple-value-bind (game-id winners) (parse-line line)
+    (declare (ignore game-id))
+    (cond
+      ((zerop winners) 0)
+      ((= 1 winners) 1)
+      (t (expt 2 (1- winners))))))
+
+(defun parse-line (line)
+  (destructuring-bind (game-segment data-segment) (cl-ppcre:split ":\\s+" line)
+    (destructuring-bind (winning-segment my-segment) (str:split "|" data-segment)
+      (let ((winning-cards  (coerce (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim winning-segment))) 'vector))
+            (my-cards       (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim my-segment)))))
+        (let ((game-id (parse-integer (cadr (cl-ppcre:split "\\s+" game-segment))))
+              (winners (loop for card in my-cards counting (s:true (find card winning-cards)))))
+          (values game-id  winners))))))
 
 (define-test example-cases-work ()
   (assert-equal 8 (process-line  "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"))
@@ -26,3 +44,6 @@
 
 (define-test day4-part-one-works ()
   (assert-equal 25183 (day4-part-one)))
+
+(define-test day4-part-two-works ()
+  (assert-equal 5667240 (day4-part-two)))
