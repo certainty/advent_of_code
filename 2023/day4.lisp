ffced93d9ad6061e4644979a87d5474686338847
certainty <david.krentzlin@gmail.com>
2023-12-04 17:09:37 +0100

    use set-intersection
    This appears to be actually faster then the previous version

--- 2023/day4.lisp
+++ 2023/day4.lisp
@@ -8,25 +8,27 @@
         sum (if (>= winners 1) (expt 2 (1- winners)) winners)))
 
 (defun day4-part-two ()
-  (let ((instances (make-array *table-size* :initial-element 1)))
+  (let ((instances (make-array *table-size* :initial-element 1 :element-type 'fixnum)))
     (dolist (line (file-lines #p"input/day4.input"))
       (multiple-value-bind (winners game-id) (parse-line line)
-        (let ((copies (aref instances (1- game-id))))
-          (when (and (> copies 0) (> winners 0))
+        (let ((copies (aref instances (1- (the fixnum game-id)))))
+          (when (and (> copies 0) (> (the fixnum winners) 0))
             ;; forward propagate the copies
             (dotimes (c copies)
               (loop for i from game-id to (min (+ game-id (1- winners)) (1- *table-size*)) ; don't execeed table boundaries
                     do (incf (aref instances i))))))))
     (reduce #'+ instances)))))
 
+(-> parse-line (string) (values integer integer))
 (defun parse-line (line)
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
   (destructuring-bind (game-segment data-segment) (cl-ppcre:split ":\\s+" line)
     (destructuring-bind (winning-segment my-segment) (str:split "|" data-segment)
-      (let ((winning-cards  (coerce (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim winning-segment))) 'vector))
+      (let ((winning-cards  (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim winning-segment))))
             (my-cards       (mapcar #'parse-integer (cl-ppcre:split "\\s+" (str:trim my-segment)))))
         (let ((game-id (parse-integer (cadr (cl-ppcre:split "\\s+" game-segment))))
-              (winners (loop for card in my-cards counting (s:true (find card winning-cards)))))
-          (values winners game-id))))))
+              (winners (length (intersection winning-cards my-cards :test #'=))))
+          (values (the fixnum winners) (the fixnum game-id)))))))
 
 (define-test day4-part-one-works ()
   (assert-equal 25183 (day4-part-one)))
